241ddaab9e759bc21533495e5f98933cdca8baea
Liangji <907015000@163.com>
2023-12-22 17:14:01 +0800

    la01 OK

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (!head) return 0; 
+    
+    node* tortoise, *hare;
+    tortoise = hare = head;
+
+    while (hare)
+    {
+        /* advance hare by 2 steps */
+        hare = hare->next;
+        if (hare) hare = hare->next;
+        /* advance tortoise by 1 step */
+        tortoise = tortoise->next;
+        /* there is a cycle */
+        if (hare == tortoise) return 1;
+    }
+
     return 0;
 }
